8dfc45a231dd21f3481209e8ccf95f57839dbbfa
matt-fang <matthewfang.calif@gmail.com>
2026-02-13 12:46:36 -0800

    replaced all firebase references - wip activity service

---
+++ mailbox/Services/ActivityService.swift
@@ -0,0 +1,14 @@
+//
+//  ActivityService.swift
+//  mailbox
+//
+//  Created by Matthew Fang on 2/13/26.
+//
+
+import Foundation
+import FirebaseCore
+import FirebaseDatabase
+
+final class ActivityService {
+    var user: TalkboxUser
+}
